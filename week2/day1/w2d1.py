003d8fecef028f6d843cbae336ceae0886bbcb85
firemeadow <lmi12@case.edu>
2021-02-23 19:12:23 -0500

    formatted folders to include weeks, added week 2 day 1 assignment

---
+++ week2/day1/w2d1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Lucas Invernizzi Week 2 Day 1 Assignment
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+np.zeros(10)
+
+
+# In[3]:
+
+
+np.ones(10)
+
+
+# In[6]:
+
+
+np.full(10, 3)
+
+
+# In[7]:
+
+
+np.arange(10,51)
+
+
+# In[8]:
+
+
+np.arange(10,51,2)
+
+
+# In[9]:
+
+
+np.arange(0,9).reshape(3,3)
+
+
+# In[10]:
+
+
+np.eye(3)
+
+
+# In[12]:
+
+
+np.random.rand()
+
